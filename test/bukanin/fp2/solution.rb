1d069aba0fcbdb0e147eaf7ff297856928b1a615
Andrey Bukanin <bukaninandrei@gmail.com>
2017-03-15 19:09:56 +0300

    fp2 partly: no reduce method

--- test/bukanin/fp2/solution.rb
+++ test/bukanin/fp2/solution.rb
@@ -1,4 +1,4 @@
-module Template
+module Bukanin
   module Fp2
     class MyArray < Array
       # Использовать стандартные функции массива для решения задач нельзя.
@@ -6,18 +6,33 @@ module Template
 
       # Написать свою функцию my_each
       def my_each
+        for elem in self
+          yield(elem)
+        end
+        self
       end
 
       # Написать свою функцию my_map
       def my_map
+        result = MyArray.new
+        my_each do |elem|
+          result.append(yield(elem))
+        end
+        result
       end
 
       # Написать свою функцию my_compact
       def my_compact
+        result = MyArray.new
+        my_each do |elem|
+          result.append(elem) if elem
+        end
+        result
       end
 
       # Написать свою функцию my_reduce
       def my_reduce
+
       end
     end
   end
